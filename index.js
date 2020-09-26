8df4425ea096bf99577d6d769a0b5dee49926a29
Scott Hoisington <hoyzington@gmail.com>
2020-09-26 02:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const titleCased = () => {
   return tutorials.map((tutorial) => {
       tutorial = tutorial.split(' ');              // ['what', 'is', 'JSONP?']
       for (let word of tutorial) { 
-        word = word[0].toUpperCase() + word.slice(1);
+        word = word.charAt(0).toUpperCase() + word.slice(1);
       }
       return tutorial.join(' ');   // 'What Is JSONP?'
     });
