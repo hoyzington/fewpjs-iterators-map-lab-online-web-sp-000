9dcfc6b9c9f32cc7e1311e91c14733bc57b8a745
Scott Hoisington <hoyzington@gmail.com>
2020-09-26 02:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials.map((tute) => {
+  return tutorials.map((tutorial) => {
       tutorial = tutorial.split(' ');              // ['what', 'is', 'JSONP?']
       for (let word of tutorial) { 
         word = word[0].toUpperCase() + word.slice(1);
